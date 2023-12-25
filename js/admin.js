308577b72b5b8383a1cf55eef1aff640ef64636c
Anton <anton.bakaliar@gmail.com>
2023-12-25 22:25:42 +0200

    server

---
+++ js/admin.js
@@ -0,0 +1,61 @@
+
+function addEmployee() {
+  const image = document.getElementById('image').value;
+  const position = document.getElementById('position').value;
+  const name = document.getElementById('name').value;
+
+  const data = { image, position, name };
+
+  fetch('http://localhost:3000/api/employees', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+    fetchAndDisplayEmployeesAdmin();
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
+
+function fetchAndDisplayEmployeesAdmin() {
+  fetch('http://localhost:3000/api/employees')
+    .then(response => response.json())
+    .then(employees => {
+      const employeeListAdmin = document.getElementById('employeeListAdmin');
+      employeeListAdmin.innerHTML = '';
+
+      employees.forEach(employee => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+          <strong>${employee.name}</strong> - ${employee.position}
+          <button onclick="deleteEmployee(${employee.id})">Delete</button>
+        `;
+        employeeListAdmin.appendChild(listItem);
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+fetchAndDisplayEmployeesAdmin();
+
+function deleteEmployee(id) {
+  fetch(`http://localhost:3000/api/employees/${id}`, {
+    method: 'DELETE',
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+    fetchAndDisplayEmployeesAdmin();
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
