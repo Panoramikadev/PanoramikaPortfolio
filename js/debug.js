91e82a155c6a89f78e8d9edf1f7ba0f7623cdc0f
Anton <panoramika.dev@gmail.com>
2024-02-01 19:48:39 +0200

    Launch config

--- js/debug.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const video = document.querySelector('video');
-
-    video.addEventListener('error', function () {
-        console.error('Error loading the video:', video.error);
-    });
-});
